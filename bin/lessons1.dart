b02894feb3cc604cea0bef138168de31be487dc3
torobekpratov <torobek.pratov@gmail.com>
2024-01-23 19:27:53 +0600

    first commit

---
+++ bin/lessons1.dart
@@ -0,0 +1,15 @@
+
+
+void main(){
+String myString ='Hello,Dart'; 
+print(myString.runtimeType);
+String myDart = "Dart programming language";
+String dart = "dart";
+print(myDart.contains(dart));
+
+String dartIsFun = "dart is fun";
+print (dartIsFun.toUpperCase());
+String a = "Hello";
+String b = "Dart";
+print (a + b);
+}
---
+++ test/lessons1_test.dart
@@ -0,0 +1,8 @@
+import 'package:lessons1/lessons1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

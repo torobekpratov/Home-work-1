eaa645c338ba58b97498a1866a1dec8ef419bf2d
torobekpratov <torobek.pratov@gmail.com>
2024-01-26 21:37:55 +0600

    added homework

--- lib/lessons1.dart
+++ lib/lessons1.dart
@@ -1,3 +1,34 @@
-int calculate() {
-  return 6 * 7;
+void main(){
+String myString ="Hello,Dart"; 
+print(myString.runtimeType);
+
+String myDart = "Dart programming language";
+String dart = "dart";
+print(myDart.contains(dart));
+
+String dartIsFun = "dart is fun";
+print (dartIsFun.toUpperCase());
+
+String dartIsCool = "DART IS COOL";
+print (dartIsCool.toLowerCase()); 
+
+String a = "Hello";
+String b = "Dart";
+print (a + b);
+
+String f = "  ";
+print(f.isNotEmpty);
+
+String dartProgramming = "Dart Programming";
+print  (dartProgramming.length);
+
+String trimMe = " Trim me ";
+print(trimMe);
+
+String replace = "Replace spaces with underscores";
+print (replace.replaceAll(" ", "_"));
+
+String e = "  ";
+print (e.isEmpty);
+
 }
